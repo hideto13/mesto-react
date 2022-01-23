261cc398b11ce95d9d3169c3bd19a68545e9e5d5
hideto13 <pinki_00@mail.ru>
2022-01-23 22:49:47 +0300

    Get user info

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,32 @@
-import avatar from "../images/kusto.jpg";
+import React from "react";
+import { api } from "../utils/api";
 
 function Main({
   handleAddPlaceClick,
   handleEditAvatarClick,
   handleEditProfileClick,
 }) {
+  const [userName, setUserName] = React.useState("");
+  const [userDescription, setUserDescription] = React.useState("");
+  const [userAvatar, setUserAvatar] = React.useState("");
+
+  React.useEffect(() => {
+    api
+      .getUserInfo()
+      .then(({ name, about, avatar }) => {
+        setUserName(name);
+        setUserDescription(about);
+        setUserAvatar(avatar);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <main>
       <section className="profile page__profile">
         <div className="profile__container">
           <div className="profile__avatar">
-            <img className="profile__image" src={avatar} alt="Аватар" />
+            <img className="profile__image" src={userAvatar} alt="Аватар" />
             <button
               className="profile__avatar-button"
               type="button"
@@ -19,14 +35,14 @@ function Main({
           </div>
           <div className="profile__info">
             <div className="profile__wrapper">
-              <h1 className="profile__title"></h1>
+              <h1 className="profile__title">{userName}</h1>
               <button
                 className="profile__edit-button"
                 type="button"
                 onClick={handleEditProfileClick}
               ></button>
             </div>
-            <p className="profile__subtitle"></p>
+            <p className="profile__subtitle">{userDescription}</p>
           </div>
         </div>
         <button
