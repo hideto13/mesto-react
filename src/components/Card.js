c014ede38dfd8656fb35dc2e3d73303a985c5f02
hideto13 <pinki_00@mail.ru>
2022-02-06 14:40:19 +0300

    Add user context in card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,18 @@
-function Card({ name, link, likes, onCardClick }) {
+import React from "react";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
+
+function Card({ name, link, likes, owner, onCardClick }) {
+  const currentUser = React.useContext(CurrentUserContext);
+  const isOwn = owner._id === currentUser._id;
+  const isLiked = likes.some((i) => i._id === currentUser._id);
+
+  const cardDeleteButtonClassName = `card__delete-button ${
+    isOwn ? "card__delete-button_active" : ""
+  }`;
+  const cardLikeButtonClassName = `card__like-button ${
+    isLiked ? "card__like-button_active" : ""
+  }`;
+
   function handleClick() {
     onCardClick({ name, link });
   }
@@ -14,11 +28,11 @@ function Card({ name, link, likes, onCardClick }) {
       <div className="card__description">
         <h2 className="card__text">{name}</h2>
         <div className="card__like-container">
-          <button className="card__like-button" type="button"></button>
+          <button className={cardLikeButtonClassName} type="button"></button>
           <p className="card__like-text">{likes.length}</p>
         </div>
       </div>
-      <button className="card__delete-button" type="button"></button>
+      <button className={cardDeleteButtonClassName} type="button"></button>
     </li>
   );
 }
